1e44a3d38abf0f7524a91a9a8a2250e6795b6991
逆生树 <923184091@qq.com>
2023-02-25 17:13:34 +0800

    使用vo优化，字面值处理

---
+++ swm-framework/src/main/java/com/swm/constants/SystemConstants.java
@@ -0,0 +1,17 @@
+package com.swm.constants;
+
+public class SystemConstants
+{
+    /**
+     * 文章 草稿
+     */
+    public static final int ARTICLE_STATUS_DRAFT = 1;
+    /**
+     * 文章 已发布
+     */
+    public static final int ARTICLE_STATUS_NORMAL = 0;
+    /**
+     * 热门文章展示数量
+     * */
+    public static final int HOT_ARTICLE_COUNTS = 10;
+}
--- swm-framework/src/main/java/com/swm/service/impl/ArticleServiceImpl.java
+++ swm-framework/src/main/java/com/swm/service/impl/ArticleServiceImpl.java
@@ -3,13 +3,18 @@ package com.swm.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.swm.constants.SystemConstants;
 import com.swm.domain.ResponseResult;
 import com.swm.domain.entity.Article;
+import com.swm.domain.vo.HotArticleVo;
 import com.swm.mapper.ArticleMapper;
 import com.swm.service.ArticleService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> implements ArticleService {
@@ -19,13 +24,20 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
         //查询热门文章
         LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
         //必须是已发布的文章
-        queryWrapper.eq(Article::getStatus, 0);
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
         //按浏览量降序
         queryWrapper.orderByDesc(Article::getViewCount);
         //最多查十条
-        Page<Article> page = new Page<>(1,10);
+        Page<Article> page = new Page<>(1, SystemConstants.HOT_ARTICLE_COUNTS);
         page(page,queryWrapper);
         List<Article> articles = page.getRecords();
-        return ResponseResult.okResult(articles);
+        List<HotArticleVo> articleVos = new ArrayList<>();
+        for (Article article : articles){
+            HotArticleVo vo = new HotArticleVo();
+            BeanUtils.copyProperties(article,vo);
+            articleVos.add(vo);
+        }
+
+        return ResponseResult.okResult(articleVos);
     }
 }
